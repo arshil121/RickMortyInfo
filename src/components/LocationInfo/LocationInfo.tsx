6bb1a4b593aa0a266716fb781dd1674076567148
shoab-alam <arshilalam121@gmail.com>
2023-11-20 08:04:21 +0600

    files added

--- src/components/LocationInfo/LocationInfo.tsx
+++ src/components/LocationInfo/LocationInfo.tsx
@@ -5,15 +5,12 @@ interface Props {
   character: Character;
 }
 
-const LocationInfo: React.FC<Props> = ({ character }) => {
+const LocationInfo = ({ character }: Props) => {
   if (character.location) {
     return (
       <div>
         <Text color="#9E9E9E">Last known location:</Text>
-        <Text
-          fontSize="lg"
-          _hover={{ color: "#FF9800", cursor: "pointer" }} 
-        >
+        <Text fontSize="lg" _hover={{ color: "#FF9800", cursor: "pointer" }}>
           {character.location.name}
         </Text>
       </div>
